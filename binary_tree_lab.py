2c6c8748755e44102f389ad29a5cce79fd472cdf
Garrett Pfingston <gpfingston@gmail.com>
2025-12-23 13:16:44 +0100

    added logic and passed tests

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -6,10 +6,23 @@ class TreeNode:
         self.left: Optional['TreeNode'] = None
         self.right: Optional['TreeNode'] = None
 
-# TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if root is None:
+        return 0
+    
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    
+    return 1 + max(left_depth, right_depth)
 
-# TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    # If both nodes are smaller than root, LCA is in left subtree
+    if p.val < root.val and q.val < root.val:
+        return lowest_common_ancestor(root.left, p, q)
+    
+    # If both nodes are greater than root, LCA is in right subtree
+    if p.val > root.val and q.val > root.val:
+        return lowest_common_ancestor(root.right, p, q)
+    
+    # Otherwise, root is the LCA (one node is on each side, or one is the root)
+    return root
